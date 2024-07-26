66e21963e64e1a95018df5ec6b03fc8981722897
boraalpsesli <boraalpsesli@hotmail.com>
2024-07-26 18:14:55 +0300

    implements landing page

---
+++ app/src/main/java/com/example/hayatwallet/scenes/LandingFragment.kt
@@ -0,0 +1,49 @@
+package com.example.hayatwallet.scenes
+
+import android.os.Bundle
+import android.text.Layout
+import android.text.SpannableStringBuilder
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.text.bold
+import androidx.navigation.fragment.findNavController
+import com.example.hayatwallet.R
+import com.example.hayatwallet.databinding.FragmentLandingBinding
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+
+class LandingFragment : Fragment() {
+
+    private lateinit var binding:FragmentLandingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLandingBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ForwardButton.setOnClickListener {
+            findNavController().navigate(
+                LoginFragmentDirections.toLoginActivity()
+            )
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() =
+            LandingFragment()
+    }
+}
