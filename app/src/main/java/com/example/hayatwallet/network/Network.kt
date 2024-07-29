f095a078a062c786779b52551d0db9ce0de38a07
Finartz <finartz@Kubra.local>
2024-07-29 10:16:26 +0300

    starts login function implementation

---
+++ app/src/main/java/com/example/hayatwallet/network/Network.kt
@@ -0,0 +1,32 @@
+package com.example.hayatwallet.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Network {
+    private const val BASE_URL = "https://staging-api.cusp.link"
+
+    private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client: OkHttpClient = OkHttpClient.Builder().apply {
+        addInterceptor(interceptor)
+    }.build()
+
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    val service: ApiInterface by lazy {
+        retrofit.create(ApiInterface::class.java)
+    }
+
+}
